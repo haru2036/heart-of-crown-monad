1341a623956f648dc9bbbbc28c0b5dbf9852d65b
haru2036 <haru2036@gmail.com>
2014-12-02 20:07:19 +0900

    added basic htcr functions

--- hocm.hs
+++ hocm.hs
@@ -1,15 +1,42 @@
 import Control.Monad.Trans.State.Lazy
+import Control.Monad.Trans.Class
 
 data Field = Field {
-                    inheritanceRights :: Int 
-                    hand :: Cards
-                    deck :: Cards
+                    inheritanceRights :: Int,
+                    hand :: Cards,
+                    deck :: Cards,
+                    trash :: Cards
                    }
 
 type Cards = [Card]
 
-type Card a = a -> Either a a
+type Card = Coins -> StateT Field (Either (Coins, Field)) Coins
 
-type TActionPhase a = StateT Field (Either a) a
+type Coins = Int
 
-data OActionPhase a = Running Field a | Finished Field a
+type ActionPhaseT = StateT Field (Either (Coins, Field)) Coins
+
+data ActionPhaseO a = Running Field a | Finished Field a
+
+farm :: Card
+farm c = do
+  return (c + 1)
+
+alchemist :: Card
+alchemist c = do
+  f <- get
+  return c
+
+stop :: Card
+stop c = do
+  fld <- get
+  lift $ Left (c, fld)
+
+dummyField = Field 0 [] [] [] 
+
+testRun = runStateT (farm 0 >>= stop >>= farm) dummyField
+
+zeroCoins = 0 :: Coins
+
+printEither (Right (c, _)) = print ("Right " ++ show c)
+printEither (Left (c, _)) = print ("Left " ++ show c)
